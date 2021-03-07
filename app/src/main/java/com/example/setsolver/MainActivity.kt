90148d7abdd267668f82d8200aa858bc295f010f
George Vida <vidagyorgy@gmail.com>
2021-03-07 18:01:25 +0000

    opencv working cannot believe it

--- app/src/main/java/com/example/setsolver/MainActivity.kt
+++ app/src/main/java/com/example/setsolver/MainActivity.kt
@@ -1,11 +1,125 @@
 package com.example.setsolver
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceView
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import org.opencv.android.CameraBridgeViewBase
+import org.opencv.android.*
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity(), CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private var mOpenCvCameraView: CameraBridgeViewBase? = null
+
+    private val mLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    Log.i(TAG, "OpenCV loaded successfully")
+
+                    // Load native library after(!) OpenCV initialization
+                    OpenCVLoader.initDebug();
+
+                    mOpenCvCameraView!!.enableView()
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "called onCreate")
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        // Permissions for Android 6+
+        ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST
+        )
+
         setContentView(R.layout.activity_main)
+
+        mOpenCvCameraView = findViewById<CameraBridgeViewBase>(R.id.main_surface)
+
+        mOpenCvCameraView!!.visibility = SurfaceView.VISIBLE
+
+        mOpenCvCameraView!!.setCvCameraViewListener(this)
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // mOpenCvCameraView!!.setCameraPermissionGranted()
+                } else {
+                    val message = "Camera permission was not granted"
+                    Log.e(TAG, message)
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+            }
+            else -> {
+                Log.e(TAG, "Unexpected permission request")
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView!!.disableView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization")
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback)
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!")
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView!!.disableView()
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {}
+
+    override fun onCameraViewStopped() {}
+
+    override fun onCameraFrame(frame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
+        // get current camera frame as OpenCV Mat object
+        val mat = frame.rgba()
+
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2BGRA)
+
+        val matCopy = mat.clone()
+        Imgproc.Canny(mat, matCopy, 80.0, 90.0)
+        // return processed frame for live preview
+        return mat
+    }
+
+    companion object {
+
+        private const val TAG = "MainActivity"
+        private const val CAMERA_PERMISSION_REQUEST = 1
     }
 }
