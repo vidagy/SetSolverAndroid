30aea887d387d892e9aa957254b6d4b28a293bdc
George Vida <vidagyorgy@gmail.com>
2021-03-08 15:42:33 +0000

    test support

--- app/src/test/java/com/example/setsolver/BoardDetectionTest.kt
+++ app/src/test/java/com/example/setsolver/BoardDetectionTest.kt
@@ -3,17 +3,21 @@ package com.example.setsolver
 import com.example.setsolver.detection.BoardDetection
 import org.junit.Assert
 import org.junit.Test
-import org.opencv.android.OpenCVLoader
+// import org.opencv.android.OpenCVLoader
 import org.opencv.core.Core
 import org.opencv.core.Mat
 import org.opencv.imgcodecs.Imgcodecs
 
 
 class BoardDetectionTest {
+    init {
+        // OpenCVLoader.initDebug();
+        nu.pattern.OpenCV.loadShared()
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
     @Test
-    fun tryLoadingASimpleImage(){
-        OpenCVLoader.initDebug();
-        val file = "src/test/java/com/example/setsolver/board_input/set_board0.jpg"
+    fun tryLoadingASimpleImage() {
+        val file = "src/test/java/com/example/setsolver/board_input/set_board1.jpg"
         val matrix: Mat = Imgcodecs.imread(file)
         val board = BoardDetection(matrix)
         val res = board.extractCardsFromBoard()
